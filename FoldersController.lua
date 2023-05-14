82aa26ebbe8ba1ca7a4407f05ce68fe22d6df6ad
MankaUser <122668564+MankaUser@users.noreply.github.com>
2023-05-14 17:52:08 +0300

    Create FoldersController.lua

---
+++ FoldersController.lua
@@ -0,0 +1,17 @@
+-- very useful stroke lol
+
+if not isfolder("ManaV2") then
+makefolder("ManaV2")
+end
+
+if not isfolder("ManaV2/Assets") then
+makefolder("ManaV2/Assets")
+end
+
+if not isfolder("ManaV2/Garbadge") then
+makefolder("ManaV2/Garbadge")
+end
+
+if not isfolder("ManaV2/CustomModules") then
+makefolder("ManaV2/CustomModules")
+end
