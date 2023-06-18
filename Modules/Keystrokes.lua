a4dff0423acbcda3a3b3f377dae1d75089cf780c
MankaUser <122668564+MankaUser@users.noreply.github.com>
2023-06-18 20:58:12 +0300

    Update Keystrokes.lua

--- Modules/Keystrokes.lua
+++ Modules/Keystrokes.lua
@@ -5,6 +5,9 @@ local keyconnection1
 local keyconnection2
 local inputService = game:GetService("UserInputService")
 local tweenService = game:GetService("TweenService")
+local guiScreen = Instance.new("ScreenGui")
+local TextLabel = Instance.new("TextLabel")
+local UICorner = Instance.new("UICorner")
 local function createKeystroke(keybutton, pos, pos2)
 	local key = Instance.new("Frame")
 	key.Size = keybutton == Enum.KeyCode.Space and UDim2.new(0, 110, 0, 24) or UDim2.new(0, 34, 0, 36)
@@ -52,3 +55,28 @@ keyconnection2 = inputService.InputEnded:Connect(function(inputType)
 		key.Tween2:Play()
 	end
 end)
+
+		guiScreen.Parent = game.Players.LocalPlayer.PlayerGui
+		keystrokesframe = Instance.new("Frame")
+		keystrokesframe.Size = UDim2.new(0, 110,0, 106)
+		keystrokesframe.BackgroundTransparency = 1
+		keystrokesframe.Parent = guiScreen
+		keystrokesframe.Active = true
+		keystrokesframe.Draggable = true
+		createKeystroke(Enum.KeyCode.W, UDim2.new(0, 38, 0, 0), UDim2.new(0, 6, 0, 5))
+		createKeystroke(Enum.KeyCode.S, UDim2.new(0, 38, 0, 42), UDim2.new(0, 8, 0, 5))
+		createKeystroke(Enum.KeyCode.A, UDim2.new(0, 0, 0, 42), UDim2.new(0, 7, 0, 5))
+		createKeystroke(Enum.KeyCode.D, UDim2.new(0, 76, 0, 42), UDim2.new(0, 8, 0, 5))
+		createKeystroke(Enum.KeyCode.Space, UDim2.new(0, 0, 0, 83), UDim2.new(0, 25, 0, -10))
+		
+		TextLabel.Parent = keystrokesframe
+		TextLabel.BackgroundColor3 = Color3.fromRGB(90, 255, 109)
+		TextLabel.BackgroundTransparency = 0.500
+		TextLabel.Size = UDim2.new(0, 34, 0, 36)
+		TextLabel.Font = Enum.Font.SourceSans
+		TextLabel.Text = ""
+		TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+		TextLabel.TextSize = 14.000
+		
+		UICorner.CornerRadius = UDim.new(0, 4)
+		UICorner.Parent = TextLabel
