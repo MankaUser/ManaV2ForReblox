e89307eb4f3a2909ace839d4375723f75450c9ea
MankaUser <122668564+MankaUser@users.noreply.github.com>
2023-06-20 14:38:13 +0300

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,4 +1,4 @@
--- Only whitelisted users can see PrivateFeatures tab, and features of this tab.
+-- Only whitelisted users can see PrivateFeatures tab, and features of that tab.
 
 local whitelists = {
   [1] = "4705120572",
