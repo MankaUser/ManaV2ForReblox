a3d65e6c38fe2cc2341b1688a659e18902c5d143
MankaUser <122668564+MankaUser@users.noreply.github.com>
2023-06-21 11:08:39 +0300

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,6 +1,8 @@
 CoreGui = game:GetService("CoreGui")
 if game.PlaceId == 6872274481 or game.PlaceId == 8560631822 or game.PlaceId == 8444591321 then 
  loadstring(game:HttpGet("https://raw.githubusercontent.com/MankaUser/ManaV2ForReblox/main/Scripts/bedwars.lua"))()
+ wait(0.5)
+ print("[Mana]: Loaded!")
 elseif game.PlaceId == 6872265039 then
 print("[Mana]: Lobby not supported yet")
  end
