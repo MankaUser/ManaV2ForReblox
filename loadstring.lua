982f3bb77e7a0b0c2543467c56007db72981c349
MankaUser <122668564+MankaUser@users.noreply.github.com>
2023-07-10 20:21:40 +0300

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,8 +1,11 @@
 if game.PlaceId == 6872274481 or game.PlaceId == 8560631822 or game.PlaceId == 8444591321 then 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/MankaUser/ManaV2ForReblox/main/Scripts/bedwars.lua"))()
+warn("[Mana]: Loading...")
+if game:GetService("CoreGui"):FindFirsChild("ScreenGui") then
 print("[Mana]: Loaded!")
+end
 elseif game.PlaceId == 6872265039 then
 warn("[Mana]: Lobby not supported yet")
 else
-warn("[Mana]: ManaV2 does not support game with ".. game.PlaceId.." ID")
+warn("[Mana]: ManaV2 does not support game that you are in, go to bedwars match")
 end
